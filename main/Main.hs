6f0ca54365379f56605ae7d166e54906b15d1268
Nathan Moos <moosingin3space@gmail.com>
2015-07-12 18:05:01 -0400

    added basic structure for CLI executable

--- main/Main.hs
+++ main/Main.hs
@@ -1,5 +1,55 @@
 module Main where
 
+import Options.Applicative
+
+data Timeframe = Day | Week
+
+data SearchOptions = SearchOptions
+                   { optQuery :: String }
+
+data SaveOptions = SaveOptions
+                 { optRecipeId :: String }
+
+data PlanOptions = PlanOptions
+                 { optTimeframe :: Timeframe }
+
+data Options = Options Command
+
+data Command = Search SearchOptions
+             | Save SaveOptions
+             | Plan PlanOptions
+
+searchOptions :: Parser Command
+searchOptions = Search <$> (SearchOptions
+            <$> argument str
+                ( metavar "QUERY"
+               <> help "Query to submit to Food2Fork" ))
+
+saveOptions :: Parser Command
+saveOptions = undefined
+
+planOptions :: Parser Command
+planOptions = undefined
+
+options :: Parser Options
+options = Options
+   <$> subparser (command "search" (info searchOptions
+                                   (progDesc "Search through Food2Fork"))
+               <> command "save" (info saveOptions
+                                 (progDesc "Save recipe for planning"))
+               <> command "plan" (info planOptions
+                                 (progDesc "Plan a certain length of time")))
+
 -- TODO
+runApp :: Options -> IO ()
+runApp (Options (Search opt)) = undefined
+runApp (Options (Save opt))= undefined
+runApp (Options (Plan opt)) = undefined
+runApp _ = return ()
+
 main :: IO ()
-main = undefined
+main = execParser opts >>= runApp
+    where
+        opts = info (helper <*> options)
+            ( fullDesc
+           <> header "dailymenu - a meal-planning utility" )
--- src/Main.hs
+++
@@ -1,5 +0,0 @@
-module Main where
-
--- TODO
-main :: IO ()
-main = undefined
